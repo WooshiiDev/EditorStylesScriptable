e940ddd0e57cbf82ed41f8b03706685fc1458ffd
Damian Slocombe <31889435+WooshiiDev@users.noreply.github.com>
2021-01-27 04:55:57 +0000

    Now stores all GUIStyles correctly

---
+++ EditorStyleScriptable/EditorStyleScriptable/Example/ExampleGUITest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+#pragma warning disable CS0649
+
+public class ExampleGUITest : MonoBehaviour
+    {
+    [System.Serializable]
+    private class DebugValues
+        {
+        public string name;
+        public float value;
+
+        public DebugValues(string name)
+            {
+            this.name = name;
+            }
+
+        public void DrawAsGUI(GUIStyle labelStyle, GUIStyle valueStyle)
+            {
+            GUILayout.BeginHorizontal ();
+                {
+                GUILayout.Label (name, labelStyle);
+                GUILayout.Label (value.ToString(), valueStyle);
+                }
+            GUILayout.EndHorizontal ();
+            }
+        }
+
+    [SerializeField]
+    private EditorStyleScriptable editorStyles;
+
+    [SerializeField]
+    private GUIStyle headerStyle, labelStyle, valueStyle;
+
+    private DebugValues[] values = new DebugValues[3]
+        {
+        new DebugValues("Delta"),
+        new DebugValues("Frame Count"),
+        new DebugValues("Time"),
+        };
+
+    // Start is called before the first frame update
+    private void Start()
+        {
+        if (editorStyles == null)
+            {
+            Debug.LogWarning ("EditorStyles is not assigned!");
+            return;
+            }
+
+        headerStyle = editorStyles.GetStyle ("ObjectFieldThumb");
+        headerStyle.alignment = TextAnchor.MiddleCenter;
+
+        labelStyle = editorStyles.GetStyle ("BoldLabel");
+        valueStyle = editorStyles.GetStyle ("HelpBox");
+        }
+
+    // Update is called once per frame
+    private void Update()
+        {
+        values[0].value = Time.deltaTime;
+        values[1].value = Time.frameCount;
+        values[2].value = Time.time;
+        }
+
+    private void OnGUI()
+        {
+        if (editorStyles == null)
+            return;
+
+        //19 is the height of a field (normally)
+        //So 19*3 for each of the floats, then another 19 for the header
+        Rect rect = new Rect (10f, 10f, Screen.width - 20f, 19f * 4f);
+
+        //Draw group of values, can call GUI.skin still too for convenience
+        GUI.BeginGroup (rect, GUI.skin.box);
+            {
+            //Use GUILayout to auto layout
+            GUILayout.Label ("Time values", headerStyle);
+
+            foreach (var value in values)
+                value.DrawAsGUI (labelStyle, valueStyle);
+            }
+        GUI.EndGroup ();
+        }
+    }
---
+++ EditorStyleScriptable/EditorStyleScriptable/Scripts/EditorStyleScriptable.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu ()]
+public class EditorStyleScriptable : ScriptableObject, ISerializationCallbackReceiver
+    {
+    [Serializable]
+    public class GUIStyleRef
+        {
+        public string name;
+        public GUIStyle style;
+
+        public GUIStyleRef(GUIStyle style)
+            {
+            this.name = style.name;
+            this.style = style;
+            }
+        }
+
+    [SerializeField]
+    private List<GUIStyleRef> editorStyles = new List<GUIStyleRef> ();
+    private static Dictionary<string, GUIStyle> styleLookup;
+
+    /// <summary>
+    /// Current style count
+    /// </summary>
+    public int StyleCount => editorStyles.Count;
+
+    /// <summary>
+    /// Add a style to the collection.
+    /// </summary>
+    public void AddStyle(GUIStyle style)
+        {
+        if (styleLookup.ContainsKey (style.name))
+            return;
+
+        GUIStyleRef styleRef = new GUIStyleRef (style);
+        editorStyles.Add (styleRef);
+        styleLookup.Add (styleRef.name, style);
+        }
+
+    /// <summary>
+    /// Add multiple styles. Will call <see cref="AddStyle(GUIStyle)"/> for each one.
+    /// </summary>
+    public void AddStyles(IEnumerable<GUIStyle> styles)
+        {
+        foreach (var style in styles)
+            AddStyle (style);
+        }
+
+    /// <summary>
+    /// Clear all style information.
+    /// </summary>
+    public void ClearStyles()
+        {
+        editorStyles.Clear ();
+        styleLookup?.Clear ();
+        }
+
+    /// <summary>
+    /// Find a style with the given name.
+    /// </summary>
+    /// <param name="name">Name of the style to find</param>
+    public GUIStyle GetStyle(string name)
+        {
+        if (string.IsNullOrEmpty (name))
+            return null;
+
+        if (styleLookup.ContainsKey (name))
+            return styleLookup[name];
+
+        return null;
+        }
+
+    public int GetStyleIndex(string name)
+        {
+        return editorStyles.FindIndex (s => s.name == name);
+        }
+
+    /// <summary>
+    /// Return all styles found
+    /// </summary>
+    public IEnumerable<GUIStyleRef> GetStyles()
+        {
+        return editorStyles;
+        }
+
+    #region Serialization
+
+    public void OnBeforeSerialize()
+        {
+        if (styleLookup == null)
+            styleLookup = new Dictionary<string, GUIStyle> ();
+        }
+
+    public void OnAfterDeserialize()
+        {
+        if (styleLookup == null)
+            styleLookup = new Dictionary<string, GUIStyle> ();
+
+        //Add the style or update
+        foreach (var style in editorStyles)
+            {
+            if (!styleLookup.ContainsKey (style.name))
+                styleLookup.Add (style.name, style.style);
+            else
+                styleLookup[style.name] = style.style;
+            }
+        }
+
+    #endregion
+    }
